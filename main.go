5f15485c1f6b747c2162fdaf66375d2a56074599
OlivierArgentieri <olivierargentieri@gmail.com>
2023-03-26 23:56:42 -0400

    first test with cobra cli

--- main.go
+++ main.go
@@ -5,24 +5,41 @@ import (
 	"log"
 
 	"github.com/OlivierArgentieri/ligornete/proxy"
+	"github.com/spf13/cobra"
 )
 
+var Host string
+var User string
+var Password string
+var LocalProxyPort string
+
+var rootCmd = &cobra.Command{
+	Use:   "ligornette",
+	Short: "Ligornette is used to create an SSH tunnel using a local socks5 Proxy server",
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&Host, "sshhost", "H", "", "ssh host (required)")
+	rootCmd.Flags().StringVarP(&User, "sshport", "U", "", "ssh user (required)")
+	rootCmd.Flags().StringVarP(&Password, "sshpassword", "P", "", "ssh password (required)")
+	rootCmd.Flags().StringVarP(&LocalProxyPort, "port", "T", "9090", "local proxy port")
+}
+
 func main() {
-	host := "192.168.1.27:22"
-	user := "debian"
-	pwd := "toor"
-
 	proxyAddr := "localhost"
-	proxyPort := "9091"
-
+	rootCmd.Execute()
 	a := proxy.Proxy{}
 
 	s := proxy.Socks{}
 
-	conn, err := a.InitSSH(host, user, pwd)
+	conn, err := a.InitSSH(Host, User, Password)
 	if err != nil {
 		log.Fatalf("failed to tunnel init ssh conneciton : %q", err)
 	}
 
-	s.Run(conn, fmt.Sprintf("%v:%v", proxyAddr, proxyPort))
+	s.Run(conn, fmt.Sprintf("%v:%v", proxyAddr, LocalProxyPort))
 }
